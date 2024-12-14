a931366c74732b4fe1569b65fb1120766fd2dfd0
org <ogamespec@gmail.com>
2024-12-14 11:25:51 +0300

    bank4

--- Build/build.bat
+++ Build/build.bat
@@ -3,7 +3,7 @@ dasm ../src/bank0.asm -f3 -obank0.bin -lbank0.lst
 dasm ../src/bank1.asm -f3 -obank1.bin -lbank1.lst
 dasm ../src/bank2.asm -f3 -obank2.bin -lbank2.lst
 dasm ../src/bank3.asm -f3 -obank3.bin -lbank3.lst
-rem dasm ../src/bank4.asm -f3 -obank4.bin -lbank4.lst
+dasm ../src/bank4.asm -f3 -obank4.bin -lbank4.lst
 rem dasm ../src/bank5.asm -f3 -obank5.bin -lbank5.lst
 dasm ../src/bank6.asm -f3 -obank6.bin -lbank6.lst
 dasm ../src/bank7.asm -f3 -obank7.bin -lbank7.lst
